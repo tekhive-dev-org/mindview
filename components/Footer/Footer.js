477a4d2c955f4615abda6fd844951d3c044a9965
covstar <okechukwuvictor093@gmail.com>
2026-04-21 15:26:55 +0100

    feat(Contact): added email contact option and update card layout for responsiveness

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -92,20 +92,20 @@ const CONTACT_INFO = [
 		text: "+234 701 825 4622",
 		href: "tel:+2347018254622",
 	},
-	// {
-	// 	icon: (
-	// 		<svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
-	// 			<path
-	// 				strokeLinecap="round"
-	// 				strokeLinejoin="round"
-	// 				strokeWidth={1.5}
-	// 				d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-	// 			/>
-	// 		</svg>
-	// 	),
-	// 	text: "contact@mindview.com",
-	// 	href: "mailto:contact@mindview.com",
-	// },
+	{
+		icon: (
+			<svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
+				<path
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					strokeWidth={1.5}
+					d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+				/>
+			</svg>
+		),
+		text: "mindviewauto@gmail.com",
+		href: "mailto:mindviewauto@gmail.com",
+	},
 	{
 		icon: (
 			<svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
--- components/contact/ContactCards/ContactCards.js
+++ components/contact/ContactCards/ContactCards.js
@@ -27,6 +27,17 @@ const CARDS = [
 	{
 		icon: (
 			<svg viewBox="0 0 24 24" fill="currentColor" className={styles.cardSvg}>
+				<path d="M20 4H4a2 2 0 00-2 2v12a2 2 0 002 2h16a2 2 0 002-2V6a2 2 0 00-2-2zm0 2v.01L12 11 4 6.01V6h16zM4 18V8.236l7.445 4.653a1 1 0 001.11 0L20 8.236V18H4z" />
+			</svg>
+		),
+		title: "Email us",
+		desc: "Share project details and our team will respond directly.",
+		action: "mindviewauto@gmail.com",
+		href: "mailto:mindviewauto@gmail.com",
+	},
+	{
+		icon: (
+			<svg viewBox="0 0 24 24" fill="currentColor" className={styles.cardSvg}>
 				<path d="M20.01 15.38c-1.23 0-2.42-.2-3.53-.56a.977.977 0 00-1.01.24l-1.57 1.97c-2.83-1.35-5.48-3.9-6.89-6.83l1.95-1.66c.27-.28.35-.67.24-1.02-.37-1.11-.56-2.3-.56-3.53 0-.54-.45-.99-.99-.99H4.19C3.65 3 3 3.24 3 3.99 3 13.28 10.73 21 20.01 21c.71 0 .99-.63.99-1.18v-3.45c0-.54-.45-.99-.99-.99z" />
 			</svg>
 		),
